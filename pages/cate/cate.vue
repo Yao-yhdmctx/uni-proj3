727ca4a1000828ec52821fe549d21c0842c49489
yaobo <1654413105@qq.com>
2023-03-15 10:08:46 +0800

    完成了搜索页面的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,9 +1,13 @@
 <template>
 	<view>
+		<view class="search-box">
+		  <my-search @click="gotoSearch"></my-search>
+		</view>
 		<view class="scroll-view-container">
 			<scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
 				<block v-for="(item,i) in cateList" :key="i">
-					<view :class="['left-scroll-view-item',i === active ? 'active' : '']" @click="activeChanged(i)">{{item.cat_name}}</view>
+					<view :class="['left-scroll-view-item',i === active ? 'active' : '']" @click="activeChanged(i)">
+						{{item.cat_name}}</view>
 				</block>
 			</scroll-view>
 			<scroll-view class="right-scroll-view" scroll-y :style="{height:wh + 'px'}" :scroll-top="scrollTop">
@@ -12,8 +16,9 @@
 						/{{item2.cat_name}}
 					</view>
 					<view class="cate-lv3-list">
-						<view class="cate-lv3-item" v-for="(item3,i3) in item2.children" :key="i3" @click="gotoGoodsList(item3)">
-							  <image :src="item3.cat_icon.replace('dev','web')" mode="widthFix"/>
+						<view class="cate-lv3-item" v-for="(item3,i3) in item2.children" :key="i3"
+							@click="gotoGoodsList(item3)">
+							<image :src="item3.cat_icon.replace('dev','web')" mode="widthFix" />
 							<text>{{item3.cat_name}}</text>
 						</view>
 					</view>
@@ -28,91 +33,115 @@
 		data() {
 			return {
 				wh: 0,
-				cateList:[],
-				active:0,
+				cateList: [],
+				active: 0,
 				cateLevel2: [],
-				scrollTop:0
+				scrollTop: 0
 			};
 		},
 		onLoad() {
-		      // 获取当前系统的信息
-		      const sysInfo = uni.getSystemInfoSync()
-		      // 为 wh 窗口可用高度动态赋值
-		      this.wh = sysInfo.windowHeight
-			  this.getCateList()
-		    },
-			methods:{
-				async getCateList(){
-					const {data:res } = await uni.$http.get('/api/public/v1/categories')
-					if(res.meta.status !== 200)return uni.$showMsg()
-					this.cateList = res.message
-					this.cateLevel2 = res.message[0].children
-				},
-				activeChanged(i){
-					this.active = i,
+		 // 获取当前系统的信息
+			const sysInfo = uni.getSystemInfoSync()
+			// 为 wh 窗口可用高度动态赋值
+			this.wh = sysInfo.windowHeight - 50
+			this.getCateList()
+		},
+		methods: {
+			async getCateList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/categories')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.cateList = res.message
+				this.cateLevel2 = res.message[0].children
+			},
+			activeChanged(i) {
+				this.active = i
 					this.scrollTop = this.scrollTop === 0 ? 1 : 0
-					this.cateLevel2 = this.cateList[i].children
-				},
-				gotoGoodsList(item3){
-					uni.navigateTo({
-						url:'/subpkg/goods_list/goods_list?cid=' + item3.cat_id
-					})
-				}
+				this.cateLevel2 = this.cateList[i].children
+			},
+			gotoGoodsList(item3) {
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+				})
+			},
+			gotoSearch() {
+				uni.navigateTo({
+					url: '/subpkg/search/search'
+				})
 			}
+		}
 	}
 </script>
 
 <style lang="scss">
-.scroll-view-container{
-	display: flex;
-	.left-scroll-view{
-		width: 120px;
-		.left-scroll-view-item{
-			line-height: 60px;
-			background-color: #f7f7f7;
+	.scroll-view-container {
+		display: flex;
+
+		.left-scroll-view {
+			width: 120px;
+
+			.left-scroll-view-item {
+				line-height: 60px;
+				background-color: #f7f7f7;
+				text-align: center;
+				font-size: 12px;
+
+				&.active {
+					background-color: #ffffff;
+					position: relative;
+
+					&::before {
+						content: '';
+						display: block;
+						width: 3px;
+						height: 30px;
+						background-color: #c00000;
+						position: absolute;
+						left: 0;
+						top: 50%;
+						transform: translateY(-50%);
+					}
+				}
+
+			}
+		}
+
+		.cate-lv2-title {
 			text-align: center;
 			font-size: 12px;
-			&.active{
-				background-color: #ffffff;
-				position: relative;
-				&::before{
-					content: '';
-					display: block;
-					width: 3px;
-					height: 30px;
-					background-color: #c00000;
-					position: absolute;
-					left: 0;
-					top: 50%;
-					transform: translateY(-50%);
-				}
-			}
-			
+			font-weight: bold;
+			padding: 15px;
 		}
-	}
-	.cate-lv2-title{
-		text-align: center;
-		font-size: 12px;
-		font-weight: bold;
-		padding: 15px;
-	}
-	.cate-lv3-list{
-		display: flex;
-		flex-wrap: wrap;
-		.cate-lv3-item{
-			width: 33.33%;
-			margin-bottom: 10px;
+
+		.cate-lv3-list {
 			display: flex;
-			flex-direction: column;
-			align-items: center;
-		}
-		image{
-			width: 60px;
-			height: 60px;
-		}
-		text{
-			font-size:12px
+			flex-wrap: wrap;
+
+			.cate-lv3-item {
+				width: 33.33%;
+				margin-bottom: 10px;
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+			}
+
+			image {
+				width: 60px;
+				height: 60px;
+			}
+
+			text {
+				font-size: 12px
+			}
 		}
 	}
-}
+	.search-box {
+	  // 设置定位效果为“吸顶”
+	  position: sticky;
+	  // 吸顶的“位置”
+	  top: 0;
+	  // 提高层级，防止被轮播图覆盖
+	  z-index: 999;
+	}
 </style>
---
+++ subpkg/search/search.vue
@@ -0,0 +1,171 @@
+<template>
+	<view class="search-box">
+		<!-- 使用 uni-ui 提供的搜索组件 -->
+		<uni-search-bar @input="input" :radius="100" cancelButton="none" placeholder="请输入搜索内容"></uni-search-bar>
+		<view class="sugg-list" v-if="searchResults.length !== 0">
+			<view class="sugg-item" v-for="(item,i) in searchResults" :key="i" @click="gotoDetail(item.goods_id)">
+				<view class="goods-name">
+					{{item.goods_name}}
+				</view>
+				<uni-icons type="arrowright" size="16"></uni-icons>
+			</view>
+		</view>
+		<view class="history-box" v-else>
+			<view class="history-title">
+				<text>搜索历史</text>
+				<uni-icons type="trash" size="17" @click="cleanHistory"></uni-icons>
+			</view>
+			<view class="history-list">
+				<uni-tag :text="item" v-for="(item,i) in historys" :key="i" @click="gotoGoodsList(item)"></uni-tag>
+			</view>
+		</view>
+	</view>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				show: true,
+				showSync: true,
+				searchVal: "",
+				timer: null,
+				kw: '',
+				searchResults: [],
+				// 搜索关键词的历史记录
+				historyList: []
+			};
+		},
+		computed: {
+			historys() {
+				// 注意：由于数组是引用类型，所以不要直接基于原数组调用 reverse 方法，以免修改原数组中元素的顺序
+				// 而是应该新建一个内存无关的数组，再进行 reverse 反转
+				return [...this.historyList].reverse()
+			}
+		},
+		onLoad() {
+			this.historyList = JSON.parse(uni.getStorageSync('kw') || '[]')
+		},
+		methods: {
+			
+			input(e) {
+				clearTimeout(this.timer)
+				this.timer = setTimeout(() => {
+					this.kw = e
+					this.getSearchList()
+				}, 500)
+			},
+			async getSearchList() {
+				  // 判断关键词是否为空
+				  if (this.kw === '') {
+				    this.searchResults = []
+				    return
+				  }
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/qsearch', {
+					query: this.kw
+				})
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.searchResults = res.message
+				this.saveSearchHistory()
+
+			},
+			gotoDetail(goods_id) {
+				uni.navigateTo({
+					// 指定详情页面的 URL 地址，并传递 goods_id 参数
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods_id
+				})
+			},
+			saveSearchHistory() {
+				// this.historyList.push(this.kw)
+
+				// 1. 将 Array 数组转化为 Set 对象
+				const set = new Set(this.historyList)
+				// 2. 调用 Set 对象的 delete 方法，移除对应的元素
+				set.delete(this.kw)
+				// 3. 调用 Set 对象的 add 方法，向 Set 中添加元素
+				set.add(this.kw)
+				// 4. 将 Set 对象转化为 Array 数组
+				this.historyList = Array.from(set)
+				uni.setStorageSync('kw', JSON.stringify(this.historyList))
+			},
+			// 清空搜索历史记录
+			cleanHistory() {
+				// 清空 data 中保存的搜索历史
+				this.historyList = []
+				// 清空本地存储中记录的搜索历史
+				uni.setStorageSync('kw', '[]')
+			},
+			gotoGoodsList(kw) {
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?query=' + kw
+				})
+			}
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.search-box {
+		position: sticky;
+		top: 0;
+		z-index: 999;
+	}
+
+	.uni-searchbar {
+		/* #ifndef APP-NVUE */
+		display: flex;
+		/* #endif */
+		flex-direction: row;
+		position: relative;
+		padding: 16rpx;
+		/* 将默认的 #FFFFFF 改为 #C00000 */
+		background-color: #c00000;
+	}
+
+	.sugg-list {
+		padding: 0 5px;
+
+		.sugg-item {
+			font-size: 12px;
+			padding: 13px 0;
+			border-bottom: 1px solid #efefef;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			.goods-name {
+				white-space: nowrap;
+				overflow: hidden;
+				text-overflow: ellipsis;
+				margin-right: 3px;
+			}
+		}
+	}
+
+	.history-box {
+		padding: 0 5px;
+
+		.history-title {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			height: 40px;
+			font-size: 13px;
+			border-bottom: 1px solid #efefef;
+		}
+
+		.history-list {
+			display: flex;
+			flex-wrap: wrap;
+
+			.uni-tag {
+				margin-top: 5px;
+				margin-right: 5px;
+			}
+		}
+	}
+</style>
